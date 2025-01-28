385b93f7ce8039c5de342ac5c25915d5dcb8e4f2
Curtis Daniels <danielscurtis721@gmail.com>
2025-01-27 18:09:20 -0800

    week 3 attendance

--- w1/main.js
+++ w1/main.js
@@ -1,3 +1,25 @@
-console.log("Week 2-Hello World");
+console.log("Hello from inside the main.js file");
+
+// let myVar;
+const myVar = "Curtis";
+const myVArType = typeof(myVar)
+console.log("myVArType " + myVArType);
+console.log(`myVArType + ${myVArType}`);
+
+
+
+function runNow () {
+    if(myVArType === "number") {
+        console.log(`will 11 this one run`)
+        } else {
+            console.log("will 13 this one  run")
+        }
+}
+runNow ();
+runNow ();
+
+
+
+
 
 
