1fd06730fc488766586737f7316c1a35274f282d
Curtis Daniels <danielscurtis721@gmail.com>
2025-02-17 22:24:42 -0800

    code before getting help

--- w5/main.js
+++ w5/main.js
@@ -1,81 +1,111 @@
-const cfpData = [];
-
-function determineHouseSizePts(size) {
-  let houseSizePoints = 0;
-  if (size === "large") {
-    houseSizePoints = 10;
-  } else if (size === "medium") {
-    houseSizePoints = 7;
-  } else if (size === "small") {
-    houseSizePoints = 4;
-  } else if (size === "apt") {
-    houseSizePoints = 2;
+const movies = [
+  {
+    title: "Friday",
+    year:"1995",
+    rating: 9.5
+  },
+  {
+    title: "Next Friday",
+    year:"2000",
+    rating: 8.5
+  },
+  {
+    title: "Friday After Next",
+    year:"2002",
+    rating: 7.5
+  },
+  
+  ]
+function movie() {
+  for (let i = 0; i < movies.length; i++) {
+const rating = document.getElementById("movie");
+rating.textContent = `${movies[i].title} ${(movies[i].year)} has a rating of ${(movies[i].rating)}` 
+    
   }
-  return houseSizePoints;
-}
 
-function determineHouseHoldPts(numberInHousehold) {
-  let houseHoldPoints = 0;
-  if (numberInHousehold === 1) {
-    houseHoldPoints = 14;
-  } else if (numberInHousehold === 2) {
-    houseHoldPoints = 12;
-  } else if (numberInHousehold === 3) {
-    houseHoldPoints = 10;
-  } else if (numberInHousehold === 4) {
-    houseHoldPoints = 8;
-  } else if (numberInHousehold === 5) {
-    houseHoldPoints = 6;
-  } else if (numberInHousehold === 6) {
-    houseHoldPoints = 4;
-  } else if (numberInHousehold > 6) {
-    houseHoldPoints = 2;
-  }
-  return houseHoldPoints;
 }
+movie()
 
-function displayOutObj(obj) {
+
+
+// const cfpData = [];
+
+// function determineHouseSizePts(size) {
+//   let houseSizePoints = 0;
+//   if (size === "large") {
+//     houseSizePoints = 10;
+//   } else if (size === "medium") {
+//     houseSizePoints = 7;
+//   } else if (size === "small") {
+//     houseSizePoints = 4;
+//   } else if (size === "apt") {
+//     houseSizePoints = 2;
+//   }
+//   return houseSizePoints;
+// }
+
+// function determineHouseHoldPts(numberInHousehold) {
+//   let houseHoldPoints = 0;
+//   if (numberInHousehold === 1) {
+//     houseHoldPoints = 14;
+//   } else if (numberInHousehold === 2) {
+//     houseHoldPoints = 12;
+//   } else if (numberInHousehold === 3) {
+//     houseHoldPoints = 10;
+//   } else if (numberInHousehold === 4) {
+//     houseHoldPoints = 8;
+//   } else if (numberInHousehold === 5) {
+//     houseHoldPoints = 6;
+//   } else if (numberInHousehold === 6) {
+//     houseHoldPoints = 4;
+//   } else if (numberInHousehold > 6) {
+//     houseHoldPoints = 2;
+//   }
+//   return houseHoldPoints;
+// }
+
+// function displayOutObj(obj) {
  
-  console.log(obj);
-  const output = document.getElementById("output");
-  const newH2 = document.createElement("h2");
-  const newP = document.createElement("p");
-  newH2.textContent = `Your Carbon Footprint total score is ${obj.tot}!`;
-  output.appendChild(newH2);
-  newP.textContent = `This score is based on your household member(s) of ${obj.yourMember} (score: ${obj.yourMemPts})`;
-  newP.textContent += ` and a ${obj.yourHouse} size home (score ${obj.yourSizePts}).`;
-  output.appendChild(newP);
-}
+//   console.log(obj);
+//   const output = document.getElementById("output");
+//   const newH2 = document.createElement("h2");
+//   const newP = document.createElement("p");
+//   newH2.textContent = `Your Carbon Footprint total score is ${obj.tot}!`;
+//   output.appendChild(newH2);
+//   newP.textContent = `This score is based on your household member(s) of ${obj.yourMember} (score: ${obj.yourMemPts})`;
+//   newP.textContent += ` and a ${obj.yourHouse} size home (score ${obj.yourSizePts}).`;
+//   output.appendChild(newP);
+// }
 
-function start(houseHoldMembers, houseSize) {
-  const houseHoldPTS = determineHouseHoldPts(houseHoldMembers);
-  const houseSizePts = determineHouseSizePts(houseSize);
-  const total = houseHoldPTS + houseSizePts;
-  cfpData.push({
-    yourMember: houseHoldMembers,
-    yourHouse: houseSize,
-    yourMemPts: houseHoldPTS,
-    yourSizePts: houseSizePts,
-    tot: total,
-  });
-}
+// function start(houseHoldMembers, houseSize) {
+//   const houseHoldPTS = determineHouseHoldPts(houseHoldMembers);
+//   const houseSizePts = determineHouseSizePts(houseSize);
+//   const total = houseHoldPTS + houseSizePts;
+//   cfpData.push({
+//     yourMember: houseHoldMembers,
+//     yourHouse: houseSize,
+//     yourMemPts: houseHoldPTS,
+//     yourSizePts: houseSizePts,
+//     tot: total,
+//   });
+// }
 
-function displayOutput() {
-  for (obj of cfpData) {
-    console.log(obj);
-    const output = document.getElementById("output");
-    const newH2 = document.createElement("h2");
-    const newP = document.createElement("p");
-    const newH3 = document.createElement("h3");
-    newH2.textContent = `Your Carbon Footprint total score is ${obj.tot}!`;
-    output.appendChild(newH2);
-    newH3.textContent = `Based on the Number in your household and size of home.`;
-    output.appendChild(newH3);
-    newP.textContent = `This score is based on your household member(s) of ${obj.yourMember} (score: ${obj.yourMemPts})`;
-    newP.textContent += ` and a ${obj.yourHouse} size home (score ${obj.yourSizePts}).`;
-    output.appendChild(newP);
-  }
-}
+// function displayOutput() {
+//   for (obj of cfpData) {
+//     console.log(obj);
+//     const output = document.getElementById("output");
+//     const newH2 = document.createElement("h2");
+//     const newP = document.createElement("p");
+//     const newH3 = document.createElement("h3");
+//     newH2.textContent = `Your Carbon Footprint total score is ${obj.tot}!`;
+//     output.appendChild(newH2);
+//     newH3.textContent = `Based on the Number in your household and size of home.`;
+//     output.appendChild(newH3);
+//     newP.textContent = `This score is based on your household member(s) of ${obj.yourMember} (score: ${obj.yourMemPts})`;
+//     newP.textContent += ` and a ${obj.yourHouse} size home (score ${obj.yourSizePts}).`;
+//     output.appendChild(newP);
+//   }
+// }
 
 // refactor code for for loop:
 // function displayOutput() {
@@ -95,9 +125,12 @@ function displayOutput() {
 //   }
 // }
 
-start(2, "apt");
-start(5, "small");
-start(3, "medium");
-start(5, "large");
+// start(2, "apt");
+// start(5, "small");
+// start(3, "medium");
+// start(5, "large");
 
-displayOutput();
+// displayOutput();
+
+
+
