f2c801590b60d83b0d5fca273f6a52a143a2138a
Curtis Daniels <danielscurtis721@gmail.com>
2025-04-23 06:51:10 -0700

    starting code ready

---
+++ w8/cfp.js
@@ -0,0 +1,36 @@
+function determineHouseSizePts(size) {
+    let houseSizePoints = 0;
+    if (size === "Large") {
+      houseSizePoints = 10;
+    } else if (size === "Medium") {
+      houseSizePoints = 7;
+    } else if (size === "Small") {
+      houseSizePoints = 4;
+    } else if (size === "Apt") {
+      houseSizePoints = 2;
+    }
+    return houseSizePoints;
+  }
+  
+  function determineHouseHoldPts(numberInHousehold) {
+    let houseHoldPoints = 0;
+    if (numberInHousehold === 1) {
+      houseHoldPoints = 14;
+    } else if (numberInHousehold === 2) {
+      houseHoldPoints = 12;
+    } else if (numberInHousehold === 3) {
+      houseHoldPoints = 10;
+    } else if (numberInHousehold === 4) {
+      houseHoldPoints = 8;
+    } else if (numberInHousehold === 5) {
+      houseHoldPoints = 6;
+    } else if (numberInHousehold === 6) {
+      houseHoldPoints = 4;
+    } else if (numberInHousehold > 6) {
+      houseHoldPoints = 2;
+    }
+    return houseHoldPoints;
+  }
+  
+  export { determineHouseHoldPts, determineHouseSizePts };
+  
---
+++ w8/main.js
@@ -0,0 +1,35 @@
+import { renderTbl } from "./render.js";
+import { determineHouseHoldPts, determineHouseSizePts } from "./cfp.js";
+
+const FORM = document.getElementById("form");
+const OUTPUT = document.getElementById("output");
+const cfpData = [];
+
+function start(first, last, houseHoldMembers, houseSize) {
+  const houseHoldPTS = determineHouseHoldPts(houseHoldMembers);
+  const houseSizePts = determineHouseSizePts(houseSize);
+  const total = houseHoldPTS + houseSizePts;
+  cfpData.push({
+    firstName: first,
+    lastName: last,
+    houseHld: houseHoldMembers,
+    houseSze: houseSize,
+    hhPts: houseHoldPTS,
+    hsZPts: houseSizePts,
+    cfpTotal: total,
+  });
+}
+
+FORM.addEventListener(`submit`, function (e) {
+  e.preventDefault();
+  const firstName = FORM.firstname.value;
+  const lastName = FORM.lastname.value;
+  const houseHoldMembers = parseInt(FORM.housem.value);
+  const houseSize = FORM.houses.value;
+  start(firstName, lastName, houseHoldMembers, houseSize);
+  OUTPUT.innerHTML = ""
+
+
+  renderTbl(cfpData);
+  FORM.reset();
+});
